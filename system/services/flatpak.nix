5c97bc57ed665a22e01bd96ac7afa56b84b2a651
folospior <folosp2@gmail.com>
2024-08-30 10:24:43 +0200

    Revert "temp disable sober"
    This reverts commit 6497022491f96a34d8e9e59d9da54cb801614cd3.

--- system/services/flatpak.nix
+++ system/services/flatpak.nix
@@ -3,18 +3,17 @@
    services.flatpak = {
      enable = true;
      remotes = lib.mkOptionDefault [
-       {
-         name = "sober";
-         location = "https://sober.vinegarhq.org/repo";
-       }
- 
+#      {
+#        name = "sober";
+#        location = "https://sober.vinegarhq.org/repo";
+#      }
        {
          name = "flathub";
          location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
        }
      ];
     packages = [
-      { appId = "org.vinegarhq.Sober"; origin = "sober"; }
+#     { appId = "org.vinegarhq.Sober"; origin = "sober"; }
     ];
   };
 }
